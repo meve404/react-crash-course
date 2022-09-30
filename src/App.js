459bb28d0b58e9ad8f497acc9537acbe020cf4ef
meve404 <procontact325@gmail.com>
2022-09-30 17:21:23 -0500

    Working on App.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
 function App() {
+  
   return (
-    <div className="App">
+    <div className="container">
       <h1>Hello from react</h1>
+      {/* <h2>{x ? name : 'No'}</h2> x ? : == if x then ... */}
+      
     </div>
   );
 }
