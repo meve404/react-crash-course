6efc3f24709b8c46d79e06c2851475590e9597dc
meve404 <procontact325@gmail.com>
2022-10-10 17:45:17 -0500

    adding task and FaTimes

--- src/App.js
+++ src/App.js
@@ -1,14 +1,37 @@
-import { Header } from "./components/Header";
-import Tasks from "./components/Tasks";
+import { Header } from "./components/Header"
+import Tasks from "./components/Tasks"
+import { useState } from "react"
 
 function App() {
+  const [tasks, setTasks] = useState ( // [name of state, function to Update] creates a state. useState is a hook (to get data)
+        [
+            {
+                id: 1,
+                text: 'Doctors Appointment',
+                day: 'Feb 5th at 2:30pm',
+                reminder: true
+            },
+            {
+                id: 2,
+                text: 'Food Shopping',
+                day: 'Feb 6th at 1:30pm',
+                reminder: true
+            },
+            {
+                id: 3,
+                text: 'Meeting at school',
+                day: 'Feb 7th at 1:30pm',
+                reminder: true
+            },
+        ]
+    )
 
   return (
     <div className="container">
       {/* <h2>{x ? name : 'No'}</h2> x ? : == if x then ... */}
       {/* <Header tittle = 'hello'></Header> */}
       <Header></Header>
-      <Tasks></Tasks>
+      <Tasks tasks={tasks}></Tasks>
     </div>
   );
 }
--- src/components/Tasks.js
+++ src/components/Tasks.js
@@ -1,32 +1,13 @@
-const tasks = [
-    {
-        id: 1,
-        text: 'Doctors Appointment',
-        day: 'Feb 5th at 2:30pm',
-        reminder: true
-    },
-    {
-        id: 2,
-        text: 'Food Shopping',
-        day: 'Feb 6th at 1:30pm',
-        reminder: true
-    },
-    {
-        id: 3,
-        text: 'Meeting at school',
-        day: 'Feb 7th at 1:30pm',
-        reminder: true
-    },
-]
+import Task from "./Task"
 
-const Tasks = () => {
-  return (
-    <div>
-        {tasks.map((task) => (  //map() makes a list with the dictionaries made on task. The arrow function works as forloop with ()
-            <h3 key={task.id}>{task.text}</h3>
-        ))}
-    </div>
-  )
+const Tasks = ({ tasks }) => {
+    return (
+        <div>
+            {tasks.map((task) => (  //map() makes a list with the dictionaries made on task. The arrow function works as forloop with .map()
+                <Task key={task.id} task={task}></Task>
+            ))}
+        </div>
+    )
 }
 
 export default Tasks
